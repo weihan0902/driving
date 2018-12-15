6067eb10937912450a0aa9d208ea7d74d69a1bd9
weihan0902 <m1057110@g-mail.nsysu.edu.tw>
2018-12-15 11:14:16 +0800

    add US part'

--- driving.py
+++ driving.py
@@ -3,6 +3,11 @@ age = input('請輸入年齡: ')
 age = int(age)
 if country == '台灣':
 	if age >= 18:
-		print('你可以找駕照')
+		print('你可以考駕照')
+	else:
+		print('你還不能考駕照')
+elif country == '美國':
+	if age >=16:
+		print('你可以考駕照')
 	else:
 		print('你還不能考駕照')
